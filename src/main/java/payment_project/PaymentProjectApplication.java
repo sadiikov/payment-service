1a85d6d83794e294c8ef0275a8c94f6db7d238b5
sadiikov <amirsadiikov@gmail.com>
2026-02-02 03:08:04 +0500

    1st commit

---
+++ src/main/java/payment_project/PaymentProjectApplication.java
@@ -0,0 +1,13 @@
+package payment_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentProjectApplication.class, args);
+    }
+
+}
