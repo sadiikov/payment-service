d0146bde59dbfe6bc97a768907557a9180724ca8
sadiikov <amirsadiikov@gmail.com>
2026-03-12 23:09:42 +0500

    Removed repeated payments and worker time into 5min

--- src/main/java/payment_project/service/PaymentAsyncProcessor.java
+++ src/main/java/payment_project/service/PaymentAsyncProcessor.java
@@ -29,24 +29,6 @@ public class PaymentAsyncProcessor {
 
     @Async
     public void process(UUID paymentId) {
-        for (int tries = 0; tries < 3; tries++){
-            try {
-                if(tries > 0){
-                    log.warn("Retry payment {} attempt {}", paymentId, tries);
-                }
-
-                processInternal(paymentId);
-                return;
-            }catch (OptimisticLockException e){
-                try {
-                    Thread.sleep(100);
-                }catch (InterruptedException ignored){}
-            }
-        }
-    }
-
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void processInternal(UUID paymentId) {
         try {
             Payment payment = paymentRepository.findById(paymentId)
                     .orElseThrow(() -> new EntityNotFoundException("Payment not found"));
